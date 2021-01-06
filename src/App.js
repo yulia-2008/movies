8d35c7e9a2b7b58894bf408f26218b94bd71d7f2
yulia <juliana.ny2008@gmail.com>
2021-01-05 21:34:56 -0500

    limit moninations to 5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
 
   state = {
     moviesArray: [],
-    searchTerm: null
+    searchTerm: null,
+    nominates: [],
+    message: ""
   }
 
   changeHandler = event => { 
@@ -16,45 +18,84 @@ class App extends Component {
     fetch(`http://www.omdbapi.com/?s=${event.target.value}&apikey=${API_KEY}`)
     .then(response => response.json())
     .then(response => {
-      if (response.Search) 
-      {console.log("fetch", response); this.setState({moviesArray: response.Search})
+      if (response.Search) {
+         this.setState({moviesArray: response.Search})
       } 
-      else {this.setState({moviesArray: [] })
+      else {
+        this.setState({moviesArray: [] })
       }
     }) 
   }
 
 
   renderMovies = () => {
-   return this.state.moviesArray.map((movie) => 
+    return this.state.moviesArray.map(movie => 
       <> 
-        <li> {movie.Title} 
+        <li accessKey={movie.imdbID}> 
+            {movie.Title} 
             ({movie.Year}) &nbsp; 
-            <button onClick={this.clickHandler}>Nominate</button>
+            <button onClick={event => this.addNominant(event)}>Nominate</button>
         </li> 
         <br/>
-      </>) 
+      </>
+    ) 
+  }
+
+  addNominant = event => { 
+    // find movie by accessKey of the <li> element and add it to state.nominates
+    let newNominant = this.state.moviesArray.find(movie => movie.imdbID === event.target.parentNode.accessKey)
+    if (this.state.nominates.length > 4 ){
+      this.setState({message: "Only 5 nominanations are allowed."})
+    }
+
+    else {
+      this.setState({nominates: [...this.state.nominates, newNominant]
+      })
+    }
+  }
+
+  removeNominant = event => {
+    let filteredNominates =  this.state.nominates.filter(movie => movie.imdbID !== event.target.parentNode.accessKey)
+    this.setState({nominates: filteredNominates})
+    
+
+  }
+
+  renderNominates = () => {
+    return this.state.nominates.map(movie => 
+      <> 
+        <li accessKey={movie.imdbID}> 
+            {movie.Title} 
+            ({movie.Year}) &nbsp; 
+            <button onClick={event => this.removeNominant(event)}>Remove</button>
+        </li> 
+        <br/>
+      </>
+    ) 
   }
 
 
-  render() { console.log("state", this.state.moviesArray)
+  render() { console.log("state", this.state.nominates)
     return (
       <>
         <div id="centered">
           <form>
-           <input type="search" placeholder="Movie Search" onChange={this.changeHandler}></input>
+           <input type="search" placeholder=" Movie Search" onChange={this.changeHandler}></input>
           </form>
         </div>
 
         <div id="flex">
           <div id="left-container">
-                <h4 id="center">Results for "{this.state.searchTerm}" </h4>
-                <br/>
-                <ul>{this.renderMovies()}</ul>               
+              <h4 id="center">Results for "{this.state.searchTerm}" </h4>
+              <br/>
+              <ul>{this.renderMovies()}</ul>               
           </div>
 
           <div id="right-container">
-           <h4> Nominations</h4>
+              <h4> Nominations</h4>
+              <br/>
+              <ul>{this.renderNominates()}</ul> 
+              <p id="red">{this.state.message}</p>
           </div>
 
 
