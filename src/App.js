b33d7976ddbefe4585bbdc6e90f05be61e4d922d
yulia <juliana.ny2008@gmail.com>
2021-01-05 19:18:47 -0500

    some css added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from 'react';
 
@@ -7,7 +6,7 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 class App extends Component {
 
   state = {
-    moviesArray: null,
+    moviesArray: [],
     searchTerm: null
   }
 
@@ -20,26 +19,48 @@ class App extends Component {
       if (response.Search) 
       {console.log("fetch", response); this.setState({moviesArray: response.Search})
       } 
+      else {this.setState({moviesArray: [] })
+      }
     }) 
+  }
+
+
+  renderMovies = () => {
+   return this.state.moviesArray.map((movie) => 
+      <> 
+        <li> {movie.Title} 
+            ({movie.Year}) &nbsp; 
+            <button onClick={this.clickHandler}>Nominate</button>
+        </li> 
+        <br/>
+      </>) 
   }
 
 
   render() { console.log("state", this.state.moviesArray)
     return (
-      <div>
-        <form>
-          <input type="search" placeholder="Movie Search" onChange={this.changeHandler}></input>
-        </form>
-
-        <div>
-          {this.state.moviesArray ?
-            <p>Results for "{this.state.searchTerm}" </p>
-            :
-            null
-          }
+      <>
+        <div id="centered">
+          <form>
+           <input type="search" placeholder="Movie Search" onChange={this.changeHandler}></input>
+          </form>
         </div>
 
-      </div>
+        <div id="flex">
+          <div id="left-container">
+                <h4 id="center">Results for "{this.state.searchTerm}" </h4>
+                <br/>
+                <ul>{this.renderMovies()}</ul>               
+          </div>
+
+          <div id="right-container">
+           <h4> Nominations</h4>
+          </div>
+
+
+        </div>
+
+      </>
     );
   }
 }
